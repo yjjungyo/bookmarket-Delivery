68b2322ad78c3aaf085c6fa442977b0ee4719b56
jaesunyoon <jaesunyoon@sk.com>
2020-11-03 16:20:27 +0900

    init commit

---
+++ src/main/java/bookmarket/PolicyHandler.java
@@ -0,0 +1,33 @@
+package bookmarket;
+
+import bookmarket.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverPaid_Ship(@Payload Paid paid){
+
+        if(paid.isMe()){
+            System.out.println("##### listener Ship : " + paid.toJson());
+        }
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverPayCanceled_DeliveryCancel(@Payload PayCanceled payCanceled){
+
+        if(payCanceled.isMe()){
+            System.out.println("##### listener DeliveryCancel : " + payCanceled.toJson());
+        }
+    }
+
+}
